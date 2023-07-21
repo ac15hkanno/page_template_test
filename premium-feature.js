7c4efa9aeb9cd13efea26ed245556e3068c9f6d8
ac15hkanno <h-kanno@academeia15.co.jp>
2023-07-21 15:44:01 +0900

    premium feature mock

---
+++ premium-feature.js
@@ -0,0 +1,5 @@
+const featureElement = document.getElementById("premium-feature");
+
+featureElement.append("This feature if only for Premium Users please contact us to upgrade your account");
+featureElement.append("(別途用意するサーバーにAPIを発信して結果を表示するイメージ)");
+console.log("premium");
