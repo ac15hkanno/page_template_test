438352c9c62f24f38a0227bc80c399784ab4898d
ac15hkanno <h-kanno@academeia15.co.jp>
2023-07-21 16:04:48 +0900

    premium feature

--- premium-feature.js
+++ premium-feature.js
@@ -1,5 +1,8 @@
 const featureElement = document.getElementById("premium-feature");
 
+const title = document.createElement('h3')
+title.innerHTML = "Premium Feature"
+featureElement.append(title);
 featureElement.append("This feature if only for Premium Users please contact us to upgrade your account");
 featureElement.append("(別途用意するサーバーにAPIを発信して結果を表示するイメージ)");
 console.log("premium");
--- templater.js
+++ templater.js
@@ -2,7 +2,9 @@ fetch('./content.md')
   .then(response => response.text())  // レスポンスボディをテキストとして読み込む
   .then((text) => {
     const html = marked.parse(text);  // テキストをHTMLに変換
-    document.getElementById('content-area').innerHTML = html;  // 変換したHTMLをDOMに挿入
+    const content = document.createElement('div')
+    const contentArea = document.getElementById('content-area').prepend(content)
+    content.innerHTML = html;  // 変換したHTMLをDOMに挿入
   })
   .catch((error) => {
     console.error('Error:', error);
